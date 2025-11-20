93e52d868d371ba4e9cc6a5d20fbeb25aa4360e3
Artem Arakcheev <artarakcheev@gmail.com>
2025-11-20 19:27:00 +0300

    Add LOAD and SAVE commands to Storage agent

--- src/storage_v1.py
+++ src/storage_v1.py
@@ -4,6 +4,7 @@ StorageV1 Agent Archetype
 import os
 import logging
 import re
+import io
 from urllib.parse import urlparse
 from datetime import datetime
 
@@ -12,6 +13,7 @@ from pymongo import AsyncMongoClient
 from bson import ObjectId
 
 from xmpp_agent import XmppAgent
+from file_manager import FileManager
 
 logger = logging.getLogger("StorageV1")
 
@@ -26,6 +28,10 @@ class StorageV1(XmppAgent):
   '''
   StorageV1 provides storage with a database
   '''
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.file_manager = FileManager()
+
   async def start(self):
     await super().start()
     try:
@@ -33,18 +39,20 @@ class StorageV1(XmppAgent):
       self.storage = self.config.options.storage
       # logger.debug(f'self.storage: {self.storage}')
 
-
-
       self.regex_list = re.compile(self.storage.commands.get("list", '^//LIST$'))
       self.regex_get = re.compile(self.storage.commands.get("get", '^//GET\\s+(?P<key>\\S+)(?:\\s+(?P<default>.+))?$'))
       self.regex_set = re.compile(self.storage.commands.get("set", '^//SET\\s+(?P<key>\\S+)\\s+(?P<value>.+)$'))
       self.regex_append = re.compile(self.storage.commands.get("append", '^//APPEND\\s+(?P<key>\\S+)\\s+(?P<value>.+)$'))
       self.regex_delete = re.compile(self.storage.commands.get("delete", '^//DELETE\\s+(?P<key>\\S+)$'))
+      self.regex_load = re.compile(self.storage.commands.get("load", '^//LOAD\\s+(?P<key>\\S+)$'))
+      self.regex_save = re.compile(self.storage.commands.get("save", '^//SAVE\\s+(?P<key>\\S+)(?:\\s+(?P<default>.+))?$'))
       # logger.debug(f'regex_list: {self.regex_list}')
       # logger.debug(f'regex_get: {self.regex_get}')
       # logger.debug(f'regex_set: {self.regex_set}')
       # logger.debug(f'regex_append: {self.regex_append}')
       # logger.debug(f'regex_delete: {self.regex_delete}')
+      # logger.debug(f'regex_load: {self.regex_load}')
+      # logger.debug(f'regex_save: {self.regex_save}')
 
       if self.storage.driver == "mongodb":
         self.client = AsyncMongoClient(DB_URL)
@@ -66,6 +74,9 @@ class StorageV1(XmppAgent):
       logger.debug(f"prompt: {prompt}")
       # logger.debug(f'self.config.options: {self.config.options}')
       content = ''
+
+      if self.file_manager.is_shared_file_url(prompt):
+        return self.file_manager.add_file_url(prompt)
 
       if re.match(self.regex_list, prompt):
         logger.debug("LIST command")
@@ -207,6 +218,81 @@ class StorageV1(XmppAgent):
           else:
             content = " "
 
+      elif match := re.match(self.regex_load, prompt):
+        key = match.group("key")
+        logger.debug(f"LOAD command with key: {key}, reading files...")
+
+        value = ''
+        file_urls = self.file_manager.get_file_urls()
+        logger.debug(f"file_urls: {file_urls}")
+        files_iobytes = self.file_manager.get_files_iobytes()
+        # logger.debug(f"files_iobytes: {files_iobytes}")
+
+        for file_iobytes, url in zip(files_iobytes, file_urls):
+          logger.debug(f"file_iobytes: {file_iobytes}")
+          logger.debug(f"url: {url}")
+          filename = self.file_manager.get_filename_from_url(url)
+          logger.debug(f"filename: {filename}")
+          file_iobytes.name = filename
+          file_iobytes.seek(0)
+          # value += file_iobytes.getValue().decode('utf-8')
+          value += file_iobytes.read().decode('utf-8')
+
+        logger.debug(f"LOAD command with key: {key}, value: {value}")
+        now = datetime.utcnow()
+        await self.storages.update_one({
+          "userId": ObjectId(self.config.userId),
+          "namespace": self.storage.namespace,
+          "key": key,
+        }, {
+          "$set": {
+            "value": value,
+            "updatedAt": now,          # update every time
+          },
+          "$setOnInsert": {
+            "createdAt": now           # only set on insert
+          }
+        }, upsert=True)
+        if self.storage.verbose == 2:
+          content = f"Loaded key '{key}'"  # " to value '{value}'."
+        elif self.storage.verbose == 1:
+          content = "OK"
+        else:
+          content = " "
+
+      elif match := re.match(self.regex_save, prompt):
+        key = match.group("key")
+        default = match.groupdict().get("default")
+        logger.debug(f"SAVE command with key: {key}, default: {default}")
+        doc = await self.storages.find_one({
+          "userId": ObjectId(self.config.userId),
+          "namespace": self.storage.namespace,
+          "key": key,
+        })
+        if doc:
+          file_bytes = doc["value"].encode('utf-8')
+          content = "Saved value"
+        elif default is not None:
+          file_bytes = default.encode('utf-8')
+          content = "Saved default value"
+        else:
+          if self.storage.verbose == 2:
+            content = f"No entry found for key '{key}'."
+          elif self.storage.verbose == 1:
+            content = "NOT_FOUND"
+          else:
+            content = " "
+
+        if file_bytes:
+          get_url = await self.upload_file(
+            file_bytes=file_bytes,
+            filename=f"{key}.md",
+            content_type='text/markdown; charset=utf-8',
+          )
+          logger.info(f"get_url: {get_url}")
+          if reply_func:
+            reply_func(get_url)
+
       else:
         logger.warning("No valid command match")
         if self.storage.verbose == 2:
@@ -215,6 +301,8 @@ class StorageV1(XmppAgent):
           content = "UNKNOWN_COMMAND"
         else:
           content = " "
+
+      self.file_manager.clear()
 
       return content
     except Exception as e:
